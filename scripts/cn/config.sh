bfba325eb95c1c769365e52458f88281e5c2b0b8
zozyo <zzy.touhou@gmail.com>
2021-09-16 01:38:46 +0800

    update node sh

--- scripts/cn/config.sh
+++ scripts/cn/config.sh
@@ -17,18 +17,6 @@ config_show()
 
 config_set_all()
 {
-	local cores
-	while true ; do
-		cores=8
-		expr $cores + 0 &> /dev/null
-		if [ $? -eq 0 ] && [ $cores -ge 1 ] && [ $cores -le 32 ]; then
-			sed -i "6c CORES=$cores" $installdir/.env
-			break
-		else
-			printf "请输入大于1小于32的整数，该数据不正确，请重新输入！\n"
-		fi
-	done
-
 	local node_name
 	while true ; do
 		node_name=jyjf
@@ -36,39 +24,6 @@ config_set_all()
 			printf "节点名称不能包含空格，请重新输入!\n"
 		else
 			sed -i "7c NODE_NAME=$node_name" $installdir/.env
-			break
-		fi
-	done
-
-	local mnemonic=""
-	local gas_adress=""
-	local balance=""
-	while true ; do
-		mnemonic="replaceme"
-		if [ -z "$mnemonic" ] || [ $(node $installdir/console.js utils verify "$mnemonic") == "Cannot decode the input" ]; then
-			printf "请输入合法助记词,且不能为空！\n"
-		else
-			gas_adress=$(node $installdir/console.js utils verify "$mnemonic")
-			balance=$(node $installdir/console.js --substrate-ws-endpoint "wss://khala.api.onfinality.io/public-ws" chain free-balance $gas_adress 2>&1)
-			balance=$(echo $balance | awk -F " " '{print $NF}')
-			balance=$(echo "$balance / 1000000000000"|bc)
-			if [ $(echo "$balance > 0.1"|bc) -eq 1 ]; then
-				sed -i "8c MNEMONIC=$mnemonic" $installdir/.env
-				sed -i "9c GAS_ACCOUNT_ADDRESS=$gas_adress" $installdir/.env
-				break
-			else
-				printf "账户PHA小于0.1，请重新输入！\n"
-			fi
-		fi
-	done
-
-	local pool_addr=""
-	while true ; do
-		pool_addr=45Kio9yJDLyWX5yUc2QmKo4RR42oT6YvDWB6tv2HFq1fPByd
-		if [ -z "$pool_addr" ] || [ $(node $installdir/console.js utils verify "$pool_addr") == "Cannot decode the input" ]; then
-			printf "请输入合法抵押池账户地址，且不能为空！\n"
-		else
-			sed -i "10c OPERATOR=$pool_addr" $installdir/.env
 			break
 		fi
 	done
--- scripts/cn/start.sh
+++ scripts/cn/start.sh
@@ -3,9 +3,6 @@
 start()
 {
 	local node_name=$(cat $installdir/.env | grep 'NODE_NAME' | awk -F "=" '{print $NF}')
-	local cores=$(cat $installdir/.env | grep 'CORES' | awk -F "=" '{print $NF}')
-	local mnemonic=$(cat $installdir/.env | grep 'MNEMONIC' | awk -F "=" '{print $NF}')
-	local pool_address=$(cat $installdir/.env | grep 'OPERATOR' | awk -F "=" '{print $NF}')
 	if ! type docker docker-compose node yq jq curl wget unzip zip >/dev/null 2>&1; then
 		log_err "----------缺少重要依赖工具，请执行sudo phala install重新安装！----------"
 		exit 1
